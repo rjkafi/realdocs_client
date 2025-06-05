3624d111f851d33fe5ccab71c775d91bc01d095b
Abdullah All Kafi <kafi2006jk@gmail.com>
2025-06-05 09:24:09 +0600

    url chnges

--- app/utils/axiosPublic.js
+++ app/utils/axiosPublic.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  // baseURL: 'https://realdocs-backend.onrender.com',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://realdocs-backend.onrender.com',
+  // baseURL: 'http://localhost:5000',
 });
 
 export default axiosPublic;
